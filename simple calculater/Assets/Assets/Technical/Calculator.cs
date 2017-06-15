a7e143619e5f6ed5f09fd8632beea0da71540efe
Frederikja163 <fjaplv64@gmail.com>
2017-06-15 10:47:30 +0200

    Revert "Day 1"
    This reverts commit 12269b6f18bb6e3ce21415c752d48ca341250cc3.

--- simple calculater/Assets/Assets/Technical/Calculator.cs
+++ simple calculater/Assets/Assets/Technical/Calculator.cs
@@ -1,21 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Calculator : MonoBehaviour
 {
 
-    private string str;
-
-    private void Start()
-    {
-        str = "";
-    }
-
-    public void AddInput(char input)
-    {
-        str += input;
-    }
+	private void Start() 
+	{
+	
+	}
 
 }
